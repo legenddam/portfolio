6aeaef538874b4f5613db88ceda2f3abd90ba785
legenddam <techdiamond@yahoo.com>
2020-09-24 03:16:25 -0600

    Add project

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var exphbs = require('express-handlebars');
 var expressValidator = require('express-validator');
 var session = require('express-session');
-var flash = require('connect-flash');
 var multer = require('multer');
 var upload = multer({dest : './public/images/portfolio'});
-
+var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
 var app = express();
@@ -56,7 +54,11 @@ app.use(expressValidator({
     };
   }
 })); 
-app.use(flash());
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest : './public/images/portfolio'});
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host : 'localhost',
+  user : 'root',
+  password : '123456',
+  database : 'portfolio'
+
+});
+
+connection.connect();
+
+router.use(require('connect-flash')());
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('admin/index');
@@ -10,8 +25,56 @@ router.get('/add', function(req, res, next) {
   res.render('admin/add');
 });
 
-router.post('/add', function(req, res, next) {
-  res.render('admin/add');
+router.post('/add', upload.single('projectimage'), function(req, res, next) {
+  var title = req.body.title;
+  var description = req.body.description;
+  var service = req.body.service;
+  var client = req.body.client;
+  var projecturl = req.body.projecturl;
+  var projectdate = req.body.projectdate;
+
+  
+  if(req.file){
+    var projectImageName = req.file.filename;
+  }
+  else{
+    var projectImageName = "noImage.jpg";
+  }
+  
+  req.checkBody('title', 'Title field is required').notEmpty();
+  req.checkBody('service', 'Service field is required').notEmpty();
+  var errors = req.validationErrors();
+  if(errors){
+    console.log('Error');
+    res.render('admin/add',{
+      errors : errors,
+      title : title,
+      description : description,
+      service : service,
+      client : client
+    })
+  }else{
+    console.log('no Errors');
+    var project = {
+      title : title,
+      description : description,
+      service : service,
+      client : client,
+      service : service,
+      url : projecturl,
+      date : projectdate,
+      image : projectImageName
+    };
+  }
+  
+  var query = connection.query('INSERT INTO projects SET ?', project, function(err, result){
+    console.log('Error' + err);
+    console.log('Success' + result);
+  });
+  req.flash('success','The project Added');
+
+  res.redirect('/admin');
+
 });
 
 module.exports = router;
